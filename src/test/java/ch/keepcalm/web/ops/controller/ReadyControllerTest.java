c38a0f58acc4d23f874de74f2a38c68967b6806b
marzelwidmer <marzelwidmer@gmail.com>
2015-05-28 07:30:21 +0200

    change management port to 9001

--- src/test/java/ch/keepcalm/web/ops/controller/ReadyControllerTest.java
+++ src/test/java/ch/keepcalm/web/ops/controller/ReadyControllerTest.java
@@ -3,10 +3,9 @@ package ch.keepcalm.web.ops.controller;
 import ch.keepcalm.web.ops.OpsApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertEquals;
@@ -16,16 +15,15 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = OpsApplication.class)
-@IntegrationTest
-@WebAppConfiguration
+@WebIntegrationTest("server.port:0")
 public class ReadyControllerTest {
 
 
     @Test
     public void testReady() throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        assertEquals("Ready", restTemplate.getForObject(
-                "http://127.0.0.1:8080/ready", String.class));
+        assertEquals("CCBUP", restTemplate.getForObject(
+                "http://127.0.0.1:8080/ready/ready.jsp", String.class));
 
     }
 }

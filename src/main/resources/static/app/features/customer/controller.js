00bf8f1d58efd3fbeb50f89bac7f88213f787c13
marzelwidmer <marzelwidmer@gmail.com>
2015-05-29 21:57:52 +0200

    add https://github.com/jmarquis/angular-hateoas

--- src/main/resources/static/app/features/customer/controller.js
+++ src/main/resources/static/app/features/customer/controller.js
@@ -15,5 +15,57 @@ angular.module('myApp.controllers', [])
             ServiceClient.delete({ id: customerId });
             $scope.customers = ServiceClient.query();
         };
+    })
+    .controller('CustomerController', function ($log, $scope, $http, $resource) {
+        'use strict';
+        console.log("CustomerController try to get a HAL RESOURCE");
+        var url = "http://localhost:8080/hal/customer/ ";
+        /*var item = $resource(url).get(null, function () {
+            console.log("Get some resources");
+            console.log("Here's a related $resource object: ", item.resource("some-related-endpoint"));
+        });*/
+
+
+        // also works with array results from $resource(...).query()
+        var items = $resource(url).query(null, function () {
+            console.log(items);
+            angular.forEach(items, function (item) {
+                console.log("Get some resources " + item.firstname + " " + item.lastname + " ");
+
+                console.log("Here's a related $resource object: ", item.links.self);
+            });
+        });
+
     });
 
+
+
+
+
+/*
+var item = $resource("/hal/customer").get(null, function () {
+    console.log("Here's a related $resource object: ", item.resource("some-related-endpoint"));
+});
+
+// also works with array results from $resource(...).query()
+var items = $resource("/hal/customer").query(null, function () {
+    angular.forEach(items, function (item) {
+        console.log("Here's a related $resource object: ", item.resource("some-related-endpoint"));
+    });
+});
+
+
+
+
+var personResource = $resource("/hal/customer/:id");
+
+var people = personResource.query(null, function () {
+    var firstPerson = people[0];
+    var firstPersonAddresses = new HateoasInterface(firstPerson).resource("addresses").query(null, function () {
+        console.log(firstPersonAddresses);
+    });
+});
+
+
+
+*/
